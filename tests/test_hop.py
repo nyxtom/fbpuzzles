cbf46d51a9d13d8c9c60aae90d5e8bec3e20d67e
Thomas Holloway <nyxtom@gmail.com>
2011-05-16 22:27:18 -0400

    Added test case to ensure nothing is returned from print_hop on non divisible integers

--- tests/test_hop.py
+++ tests/test_hop.py
@@ -28,18 +28,25 @@ class HopTestCase(TestCase):
         Test case for integers divisible by three
         """
         result = self.print_hop(12)
-        assert that(result).equals('Hoppity\n'), 'For integers divisible by 3, print Hoppity\n'
+        assert that(result).equals('Hoppity'), 'For integers divisible by 3, print Hoppity'
 
     def test_hop_divisible_five(self):
         """
         Test case for integers divisible by five
         """
         result = self.print_hop(10)
-        assert that(result).equals('Hophop\n'), 'For integers divisible by 5, print Hophop\n'
+        assert that(result).equals('Hophop'), 'For integers divisible by 5, print Hophop'
 
     def test_hop_divisible_three_five(self):
         """
         Test case for integers divisible by both three and five
         """
         result = self.print_hop(15)
-        assert that(result).equals('Hop\n'), 'For integers divisible by both 3 & 5, print Hop\n'
+        assert that(result).equals('Hop'), 'For integers divisible by both 3 & 5, print Hop'
+
+    def test_hop_print_none(self):
+        """
+        Test case to ensure that print hop returns nothing on non divisible integers
+        """
+        result = self.print_hop(7)
+        assert not result, 'Print hop should print nothing on non divisible integers'
