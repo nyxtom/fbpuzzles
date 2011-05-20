cfe5c40745c86900cacb2d9337c9683feb448b69
Thomas Holloway <nyxtom@gmail.com>
2011-05-20 18:50:20 -0400

    Adding boilerplate runner for meepmeep puzzle

--- puzzles/meepmeep.py
+++ puzzles/meepmeep.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 class Puzzle(object):
     """
     Defines the Meep meep puzzle implementation.
@@ -13,3 +16,18 @@ class Puzzle(object):
         Prints the output Meep meep.
         """
         return 'Meep meep!'
+
+# Puzzle runner boilerplate
+if __name__ == "__main__":
+    import sys
+    colors = dict(
+        red="\033[1;31m",
+        nc="\033[0m"
+    )
+    if not len(sys.argv) > 1:
+        print '%sYou must provide an input filename%s' % (colors['red'], colors['nc'])
+    else:
+
+        # Since this particular puzzle will throw away input, just call run with nothing
+        runner = Puzzle()
+        runner.run('')
